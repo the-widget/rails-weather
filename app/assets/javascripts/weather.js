0462b39f638148103ce263f7d028951a6bee2e7d
brennenawana108ny@gmail.com <brennenawana108ny@gmail.com>
2016-07-19 19:31:47 -0400

    user input collects weather data into arrays

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -1,26 +1,32 @@
 var API_KEY = '94badd64d670682ddf96dfc942e3575a'
 var BASE_URL = 'http://api.openweathermap.org/data/2.5/forecast'
-var QUERY = "?q=Hawaii,us&"
 
 $('.fetchWeather').submit(function(event) {
   event.preventDefault();
-    $.getJSON(BASE_URL + QUERY + "&APPID=" + API_KEY, function(data) {
+    var city = $('input').val()
+    $.getJSON(BASE_URL + "?q=" + city + ",us" + "&APPID=" + API_KEY, function(data) {
       console.log(data.list)
       parse(data.list)
-      $('.response').html(data.list[0]['main']['humidity']);
+      // $('.response').html(data.list[0]['main']['humidity']);
     })
      .done(function(){
       alert("done")
      })
      
 });
-
+var humidity = [];
+var temperature = [];
+var pressure = []
 parse = function(weatherArray) {
-  var humidity = [];
+  
   weatherArray.forEach(function(obj){
     humidity.push(obj.main.humidity);
+    temperature.push(obj.main.temp);
+    pressure.push(obj.main.pressure);
   })
   console.log(humidity)
+  console.log(temperature)
+  console.log(pressure)
 }
 
 
